a63626951e9f0b07fbe95ed0855995cc009a4369
Amlan Sahoo <amlan.sahoo.98@gmail.com>
2024-08-03 20:21:48 +0530

    initial commit for auth server

---
+++ authserver/src/test/java/com/scalerproj/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalerproj.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
